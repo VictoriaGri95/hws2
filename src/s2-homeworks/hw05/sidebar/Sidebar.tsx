0192d22a3a21e1e38c461d068fc9040b6ff37bef
VictoriaGri95 <v.grivusevich95@gmail.com>
2026-02-01 20:39:06 +0300

    01-02-2026-HW5

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -40,6 +40,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
             id={'hw5-pre-junior-link'}
             to={PATH.PRE_JUNIOR}
             onClick={handleClose}
+            className={( isActive ) => (isActive ? s.active : '')}
             // className={...} // делает студент
           >
             Pre-junior
@@ -48,6 +49,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
             id={'hw5-junior-link'}
             to={PATH.JUNIOR}
             onClick={handleClose}
+            className={( isActive ) => (isActive ? s.active : '')}
             // className={...} // делает студент
           >
             Junior
@@ -56,6 +58,7 @@ export const Sidebar: FC<PropsType> = ({open, handleClose}) => {
             id={'hw5-junior-plus-link'}
             to={PATH.JUNIOR_PLUS}
             onClick={handleClose}
+            className={( isActive ) => (isActive ? s.active : '')}
             // className={...} // делает студент
           >
             Junior Plus
